9b3448abce835de708c5a84333ae51bf3fc36b56
Pritesh <pranjalmishra0054s@gmail.com>
2024-07-05 19:45:12 +0530

    Directory creation

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const path=require('path');
+const Port=8000;
+
+
+const app=express();
+
+
+app.set('view engine','ejs');
+app.set('views','views');
+app.get('/',function(req,res){
+    res.render('home');
+});
+
+app.listen(Port,function(){
+    console.log("Running at Port:8000");
+});
